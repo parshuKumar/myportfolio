5ba67bcf2597d7464efd6374c4431a92ad08665c
ParshuKumar <learnerparshuram@gmail.com>
2026-06-18 15:55:07 +0530

    u

--- src/components/HeroSection/HeroStyle.js
+++ src/components/HeroSection/HeroStyle.js
@@ -94,20 +94,25 @@ export const HeroRightContainer = styled.div`
 
 export const Img = styled.img`
   position: relative;
-  width: 100%;
-  height: 100%;
-  max-width: 400px;
-  max-height: 400px;
+  width: 320px;
+  height: 420px;
+  max-width: 320px;
+  max-height: 420px;
   border-radius: 50%;
+  object-fit: cover;
   border: 2px solid ${({ theme }) => theme.primary};
 
   @media (max-width: 768px) {
-    max-width: 400px;
-    max-height: 400px;
+    width: 280px;
+    height: 370px;
+    max-width: 280px;
+    max-height: 370px;
   }
 
   @media (max-width: 640px) {
-    max-width: 280px;
+    width: 210px;
+    height: 280px;
+    max-width: 210px;
     max-height: 280px;
   }
 `;
