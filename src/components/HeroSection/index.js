932543dfa831f842144fb579c2a01fb16b1f9a69
ParshuKumar <learnerparshuram@gmail.com>
2026-06-18 15:47:26 +0530

    udpated

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import HeroBgAnimation from '../HeroBgAnimation'
 import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle, ResumeButton } from './HeroStyle'
-import HeroImg from '../../images/nal.jpeg'
+import HeroImg from '../../images/nal'
 import Typewriter from 'typewriter-effect';
 import { Bio } from '../../data/constants';
 
